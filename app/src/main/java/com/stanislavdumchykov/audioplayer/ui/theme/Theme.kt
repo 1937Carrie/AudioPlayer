a8fa594f84481174f447e6c8ed8a131f19bb9f08
Stanislav Dumchykov <mechanikea@gmail.com>
2023-06-06 10:00:48 +0300

    Added dependencies.

--- app/src/main/java/com/stanislavdumchykov/audioplayer/ui/theme/Theme.kt
+++ app/src/main/java/com/stanislavdumchykov/audioplayer/ui/theme/Theme.kt
@@ -17,13 +17,13 @@ import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
-    secondary = PurpleGrey80,
+    secondary = Teal200,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
+    primary = Orange200,
+    secondary = Orange200,
     tertiary = Pink40
 
     /* Other default colors to override
