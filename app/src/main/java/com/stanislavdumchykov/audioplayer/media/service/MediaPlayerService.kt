eb06a8f06cef8d975b100fcbc2aba468a1acbbaa
Stanislav Dumchykov <mechanikea@gmail.com>
2023-06-06 11:04:56 +0300

    AP-2: Set up Hilt.

--- app/src/main/java/com/stanislavdumchykov/audioplayer/media/service/MediaPlayerService.kt
+++ app/src/main/java/com/stanislavdumchykov/audioplayer/media/service/MediaPlayerService.kt
@@ -3,11 +3,20 @@ package com.stanislavdumchykov.audioplayer.media.service
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
 import androidx.media.MediaBrowserServiceCompat
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.upstream.cache.CacheDataSource
 import com.stanislavdumchykov.audioplayer.media.constants.Constants
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MediaPlayerService : MediaBrowserServiceCompat() {
+
+    @Inject
+    lateinit var dataSourceFactory: CacheDataSource.Factory
+
+    @Inject
+    lateinit var exoPlayer: ExoPlayer
     override fun onGetRoot(
         clientPackageName: String,
         clientUid: Int,
