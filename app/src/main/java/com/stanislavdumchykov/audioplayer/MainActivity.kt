f889fd2cb3444c7bfc946c376c7d84497d094195
Stanislav Dumchykov <mechanikea@gmail.com>
2023-06-07 09:24:07 +0300

    AP-8: Fixed read audio permission issue.

--- app/src/main/java/com/stanislavdumchykov/audioplayer/MainActivity.kt
+++ app/src/main/java/com/stanislavdumchykov/audioplayer/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.stanislavdumchykov.audioplayer
 
 import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,12 +10,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -34,7 +33,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             AudioPlayerTheme {
                 val permissionState = rememberPermissionState(
-                    permission = Manifest.permission.READ_EXTERNAL_STORAGE
+                    permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+                        Manifest.permission.READ_MEDIA_AUDIO
+                    else
+                        Manifest.permission.READ_EXTERNAL_STORAGE
                 )
 
                 val lifecycleOwner = LocalLifecycleOwner.current
@@ -52,9 +54,7 @@ class MainActivity : ComponentActivity() {
                     onDispose {
                         lifecycleOwner.lifecycle.removeObserver(observer)
                     }
-
                 }
-
 
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -72,8 +72,7 @@ class MainActivity : ComponentActivity() {
                             },
                             isAudioPlaying = audioViewModel.isAudioPlaying,
                             audioList = audioList,
-                            currentPlayingAudio = audioViewModel
-                                .currentPlayingAudio.value,
+                            currentPlayingAudio = audioViewModel.currentPlayingAudio.value,
                             onStart = {
                                 audioViewModel.playAudio(it)
                             },
@@ -94,18 +93,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AudioPlayerTheme {
-        Greeting("Android")
     }
 }
